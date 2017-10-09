61aac590884f21d1318c7ef3a26a3e53a375175f
Alexey Andreev <Alexey.Andreev@jetbrains.com>
2017-10-09 19:28:19 +0300

    Add collapsible menu

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(function() {
+    // Markdown plain out to bootstrap style
+    $('#markdown-content-container table').addClass('table');
+    $('#markdown-content-container img').addClass('img-responsive');
+});
